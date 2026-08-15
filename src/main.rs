03b51775a54d70bf5c9f378d2d648b40a982140b
bearbearean <bear@bearbearean.dev>
2026-08-15 22:12:29 +0200

    Add axum and tokio dependencies, setup basic main function.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,27 @@
 //!
 //! > non-breaking space: a thoughtful community forum platform
 
+use axum::{Router, response::IntoResponse, routing};
+use tokio::net::TcpListener;
+
 /// The main function for nbsp.
-pub fn main() {
+#[tokio::main]
+pub async fn main() {
     println!("non-breaking space: a thoughtful community forum platform");
+
+    let router = Router::new().route("/", routing::get(root));
+
+    let listener = TcpListener::bind("127.0.0.1:3000")
+        .await
+        .expect("bind to 127.0.0.1:3000");
+
+    println!("listening on http://127.0.0.1:3000");
+    axum::serve(listener, router)
+        .await
+        .expect("axum::serve error");
+}
+
+/// The route for `GET /` (the home page)
+pub async fn root() -> impl IntoResponse {
+    "Welcome to nbsp!"
 }
