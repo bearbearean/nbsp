c2558190820224516d14c346056382f0e1490d1e
bearbearean <bear@bearbearean.dev>
2026-08-15 21:46:27 +0200

    Initialize Cargo project at v0.0.0

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+//! # nbsp
+//!
+//! > non-breaking space: a thoughtful community forum platform
+
+/// The main function for nbsp.
+pub fn main() {
+    println!("non-breaking space: a thoughtful community forum platform");
+}
