bc8126805cafc69972094b0e8848f0544a4e5729
bearbearean <bear@bearbearean.dev>
2026-08-18 21:14:15 +0200

    feat(nbsp_config): add a way to inject extra html in the head and body

---
+++ migrations/20260818_002_extra_body_head_html.down.sql
@@ -0,0 +1,2 @@
+DELETE FROM nbsp_config
+WHERE key IN ('nbsp_html_head_extra', 'nbsp_html_body_extra');
---
+++ migrations/20260818_002_extra_body_head_html.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO nbsp_config (key, value)
+VALUES
+    ('nbsp_html_head_extra', NULL),
+    ('nbsp_html_body_extra', NULL);
