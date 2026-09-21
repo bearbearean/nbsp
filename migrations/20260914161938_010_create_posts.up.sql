2f799edfe6b040fbda1f68eacc163576b99e5025
bearbearean <bear@bearbearean.dev>
2026-09-21 21:48:49 +0200

    fix: make creator_user_id not unique for posts (#37)

--- migrations/20260914161938_010_create_posts.up.sql
+++ migrations/20260914161938_010_create_posts.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS posts (
     post_id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    creator_user_id BIGINT NOT NULL UNIQUE REFERENCES users(user_id),
+    creator_user_id BIGINT NOT NULL REFERENCES users(user_id),
     created_at TIMESTAMPTZ NOT NULL DEFAULT (now()),
     title TEXT NOT NULL,
     markdown_source TEXT NOT NULL,
